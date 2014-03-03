b92a2576b24606d1766f54a6dcff14021bf54d49
Nik Taormina <c15nikolas.taormina@usafa.edu>
2014-03-03 02:27:49 -0700

    required and almost B functionality, added a lot to top level and character gen

--- atlys_lab_font_controller.vhd
+++ atlys_lab_font_controller.vhd
@@ -21,8 +21,147 @@ end atlys_lab_font_controller;
 
 architecture Behavioral of atlys_lab_font_controller is
 
+COMPONENT character_gen
+	PORT(
+		clk : IN std_logic;
+		blank : IN std_logic;
+		reset : IN std_logic;
+		row : IN std_logic_vector(10 downto 0);
+		column : IN std_logic_vector(10 downto 0);
+		ascii_to_write : IN std_logic_vector(7 downto 0);
+		write_en : IN std_logic;          
+		r : OUT std_logic_vector(7 downto 0);
+		g : OUT std_logic_vector(7 downto 0);
+		b : OUT std_logic_vector(7 downto 0)
+		);
+	END COMPONENT;
+	
+
+	
+	COMPONENT vga_sync
+	PORT(
+		clk : IN std_logic;
+		reset : IN std_logic;          
+		h_sync : OUT std_logic;
+		v_sync : OUT std_logic;
+		v_completed : OUT std_logic;
+		blank : OUT std_logic;
+		row : OUT unsigned(10 downto 0);
+		column : OUT unsigned(10 downto 0)
+		);
+	END COMPONENT;
+	
+	COMPONENT input_to_pulse
+	PORT(
+		clk : IN std_logic;
+		reset : IN std_logic;
+		input : IN std_logic;          
+		pulse : OUT std_logic
+		);
+	END COMPONENT;
+
+	
+		signal row_connector, column_connector : unsigned(10 downto 0);
+	
+	signal red_sig, green_sig, blue_sig : std_logic_vector(7 downto 0);
+	signal enable_connector, pixel_clk, serialize_clk, serialize_clk_n, h_sync_sig, v_sync_sig, v_completed, blank_sig, red_s, green_s, blue_s, clock_s:std_logic;
+
 begin
 
+    -- Clock divider - creates pixel clock from 100MHz clock
+    inst_DCM_pixel: DCM
+    generic map(
+                   CLKFX_MULTIPLY => 2,
+                   CLKFX_DIVIDE   => 8,
+                   CLK_FEEDBACK   => "1X"
+               )
+    port map(
+                clkin => clk,
+                rst   => reset,
+                clkfx => pixel_clk
+            );
+
+    -- Clock divider - creates HDMI serial output clock
+    inst_DCM_serialize: DCM
+    generic map(
+                   CLKFX_MULTIPLY => 10, -- 5x speed of pixel clock
+                   CLKFX_DIVIDE   => 8,
+                   CLK_FEEDBACK   => "1X"
+               )
+    port map(
+                clkin => clk,
+                rst   => reset,
+                clkfx => serialize_clk,
+                clkfx180 => serialize_clk_n
+            );
+
+    -- TODO: VGA component instantiation
+	 
+
+	Inst_character_gen: character_gen PORT MAP(
+		clk => pixel_clk,
+		blank => blank_sig,
+		reset => reset,
+		row => std_logic_vector(row_connector),
+		column => std_logic_vector(column_connector),
+		ascii_to_write => switch,
+		write_en => enable_connector,
+		r => red_sig,
+		g => green_sig,
+		b => blue_sig
+	);
+	
+	
+	Inst_vga_sync: vga_sync PORT MAP(
+		clk => pixel_clk,
+		reset => reset,
+		h_sync => h_sync_sig,
+		v_sync => v_sync_sig,
+		v_completed => open,
+		blank => blank_sig,
+		row => row_connector,
+		column => column_connector
+	);
+	
+	Inst_input_to_pulse: input_to_pulse PORT MAP(
+		clk => pixel_clk,
+		reset => reset,
+		input => start,
+		pulse => enable_connector
+	); 
+
+
+    -- Convert VGA signals to HDMI (actually, DVID ... but close enough)
+    inst_dvid: entity work.dvid
+    port map(
+                clk       => serialize_clk,
+                clk_n     => serialize_clk_n, 
+                clk_pixel => pixel_clk,
+                red_p     => red_sig,
+                green_p   => green_sig,
+                blue_p    => blue_sig,
+                blank     => blank_sig,
+                hsync     => h_sync_sig,
+                vsync     => v_sync_sig,
+                -- outputs to TMDS drivers
+                red_s     => red_s,
+                green_s   => green_s,
+                blue_s    => blue_s,
+                clock_s   => clock_s
+            );
+
+    -- Output the HDMI data on differential signalling pins
+    OBUFDS_blue  : OBUFDS port map
+        ( O  => TMDS(0), OB => TMDSB(0), I  => blue_s  );
+    OBUFDS_red   : OBUFDS port map
+        ( O  => TMDS(1), OB => TMDSB(1), I  => green_s );
+    OBUFDS_green : OBUFDS port map
+        ( O  => TMDS(2), OB => TMDSB(2), I  => red_s   );
+    OBUFDS_clock : OBUFDS port map
+        ( O  => TMDS(3), OB => TMDSB(3), I  => clock_s );
+
+
+	
 
 end Behavioral;
 
--- character_gen.vhd
+++ character_gen.vhd
@@ -12,6 +12,7 @@ use UNISIM.VComponents.all;
 entity character_gen is
     port ( clk            : in std_logic;
            blank          : in std_logic;
+			  reset          : in std_logic;
            row            : in std_logic_vector(10 downto 0);
            column         : in std_logic_vector(10 downto 0);
            ascii_to_write : in std_logic_vector(7 downto 0);
@@ -22,7 +23,104 @@ end character_gen;
 
 architecture Behavioral of character_gen is
 
+COMPONENT font_rom
+	PORT(
+		clk : IN std_logic;
+		addr : IN std_logic_vector(10 downto 0);          
+		data : OUT std_logic_vector(7 downto 0)
+		);
+	END COMPONENT;
+	
+	COMPONENT char_screen_buffer
+	PORT(
+		clk : IN std_logic;
+		we : IN std_logic;
+		address_a : IN std_logic_vector(11 downto 0);
+		address_b : IN std_logic_vector(11 downto 0);
+		data_in : IN std_logic_vector(7 downto 0);          
+		data_out_a : OUT std_logic_vector(7 downto 0);
+		data_out_b : OUT std_logic_vector(7 downto 0)
+		);
+	END COMPONENT;
+
+signal sel : std_logic_vector(2 downto 0);
+signal row_sig : std_logic_vector(3 downto 0);
+signal font_connector : std_logic_vector(10 downto 0);
+signal output : std_logic;
+signal data_from_font, b_data_signal : std_logic_vector(7 downto 0);
+signal address_b_12_bit, counter, counter_helper : std_logic_vector(11 downto 0);
+signal address_b_signal : std_logic_vector(13 downto 0);
+
 begin
+
+sel <= column(2 downto 0);
+
+process(column, sel, clk)
+begin
+
+	case sel is
+		when"000"=>
+			output <= data_from_font(7);
+
+		when"001"=>
+			output <= data_from_font(6);
+
+		when"010"=>
+			output <= data_from_font(5);
+			
+		when"011"=>
+			output <= data_from_font(4);
+			
+		when"100"=>
+			output <= data_from_font(3);
+			
+		when"101"=>
+			output <= data_from_font(2);
+			
+		when"110"=>
+			output <= data_from_font(1);
+			
+		when"111"=>
+			output <= data_from_font(0);
+			
+		when others=>
+			
+	end case;
+end process;
+
+r <= (others=>'1') when output = '1' else
+		(others=>'0');
+		
+g<= (others=>'0');		
+b<= (others=>'0');
+
+font_connector <= b_data_signal(6 downto 0) & row_sig;
+row_sig <= row(3 downto 0);
+
+	Inst_font_rom: font_rom PORT MAP(
+		clk => clk,
+		addr => font_connector,
+		data => data_from_font
+	);
+	
+	Inst_char_screen_buffer: char_screen_buffer PORT MAP(
+		clk => clk,
+		we => write_en,
+		address_a => counter,
+		address_b => address_b_12_bit,
+		data_in => ascii_to_write,
+		data_out_a => open,
+		data_out_b => b_data_signal
+	);
+
+address_b_signal <= std_logic_vector(((unsigned(column(10 downto 3))) + unsigned(row(10 downto 4)) * 80));
+address_b_12_bit <= address_b_signal(11 downto 0);
+
+counter_helper <= (others => '0') when reset = '1' else
+						counter;
+
+counter <= std_logic_vector(unsigned(counter_helper) + 1) when rising_edge(write_en) else
+			  counter_helper;				
 
 
 end Behavioral;
